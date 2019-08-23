6bad9faedca982ef76ed202e2210236cf0b94908
kiki0805 <kiki_hzz@163.com>
2019-08-23 11:12:27 +0800

    animate chapters

--- makestorytogether/src/components/writing/chapterNode.js
+++ makestorytogether/src/components/writing/chapterNode.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Icon } from 'antd';
 import { deleteItem } from '../../api/items';
 import { connect } from 'react-redux';
-
+import Texty from 'rc-texty';
+import Animate from 'rc-animate';
 class ChapterNode extends React.Component {
     // constructor(props) {}
     handleDelete = () => {
@@ -15,10 +16,15 @@ class ChapterNode extends React.Component {
     render() {
         const chapter = this.props.chapter;
         return (
+            <Animate
+                transitionName="fade"
+                transitionAppear
+            >
             <div key={chapter.id}>
                 {this.props.editMode ? <Icon onClick={this.handleDelete} type="delete" style={{ marginRight: "8px" }} /> : null}
                 {chapter.title}
             </div>
+            </Animate>
         )
     }
 }
